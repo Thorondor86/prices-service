bc0280b6417bcea7e526b6d0087f1b7e94c98de2
Thorondor86 <joanguaspb@gmail.com>
2025-08-29 10:41:34 +0200

    Update PriceController.java
    Optimize imports in Controller

--- src/main/java/com/example/prices/adapter/in/rest/PriceController.java
+++ src/main/java/com/example/prices/adapter/in/rest/PriceController.java
@@ -3,9 +3,11 @@ package com.example.prices.adapter.in.rest;
 import com.example.prices.domain.model.Price;
 import com.example.prices.domain.port.in.QueryApplicablePriceUseCase;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Optional;
